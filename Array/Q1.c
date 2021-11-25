f9a9468f11b13ae3f77c0435c01467a266829962
obtusei <54235268+obtusei@users.noreply.github.com>
2021-11-25 17:33:08 +0545

    first commit

---
+++ Array/Q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Hello World\n");
+    scanf("%d", &a);
+    printf("NUmber is %d", a);
+    return 0;
+}
---
+++ Array/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Hello World\n");
+    scanf("%d", &a);
+    printf("
