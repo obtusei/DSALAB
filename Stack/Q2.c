476da2e7ad60077311361dd7a38ed0b7feef1c96
obtusei <54235268+obtusei@users.noreply.github.com>
2021-11-27 08:31:02 +0545

    added new answers

--- Stack/Q2.c
+++ Stack/Q2.c
@@ -1,66 +1,99 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
-// Program to evalute postfix expression
-int stack[20];
+#define MAXSTACK 100
+#define POSTFIXSIZE 100
+
+int stack[MAXSTACK];
 int top = -1;
 
-void push(int x)
+void push(int item)
 {
-    stack[++top] = x;
+
+    if (top >= MAXSTACK - 1)
+    {
+        printf("stack over flow");
+    }
+    else
+    {
+        top = top + 1;
+        stack[top] = item;
+    }
 }
 
 int pop()
 {
-    return stack[top--];
+    int item;
+    if (top < 0)
+    {
+        printf("stack under flow");
+        return -1;
+    }
+    else
+    {
+        item = stack[top];
+        top = top - 1;
+        return item;
+    }
+}
+
+void evalPostfix(char postfix[])
+{
+    int i;
+    char ch;
+    int val;
+    int A, B;
+
+    for (i = 0; postfix[i] != ')'; i++)
+    {
+        ch = postfix[i];
+        if (isdigit(ch))
+        {
+            // converting 'any number' to actual number
+            push(ch - '0');
+        }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+        {
+            A = pop();
+            B = pop();
+            switch (ch)
+            {
+            case '*':
+                val = B * A;
+                break;
+
+            case '/':
+                val = B / A;
+                break;
+
+            case '+':
+                val = B + A;
+                break;
+
+            case '-':
+                val = B - A;
+                break;
+            }
+            push(val);
+        }
+    }
+    printf(" \n Result of expression evaluation : %d or %d \n", pop(), stack[top + 1]);
 }
 
 int main()
 {
-    char exp[20];
-    char *e;
-    int n1, n2, n3, num;
-    printf("Enter the expression :: ");
-    scanf("%s", exp);
-    e = exp;
-    while (*e != '\0')
+
+    int i;
+    char postfix[POSTFIXSIZE];
+    printf("There are only four operators(*, /, +, -) in an expression:\n");
+    printf(" \nEnter postfix expression,\npress right parenthesis ')' for end expression : ");
+    for (i = 0; i < POSTFIXSIZE; i++)
     {
-        if (isdigit(*e))
-        {
-            num = *e - 48;
-            push(num);
-        }
-        else
-        {
-            n1 = pop();
-            n2 = pop();
-            switch (*e)
-            {
-            case '+':
-            {
-                n3 = n1 + n2;
-                break;
-            }
-            case '-':
-            {
-                n3 = n2 - n1;
-                break;
-            }
-            case '*':
-            {
-                n3 = n1 * n2;
-                break;
-            }
-            case '/':
-            {
-                n3 = n2 / n1;
-                break;
-            }
-            }
-            push(n3);
-        }
-        e++;
+        scanf("%c", &postfix[i]);
+        if (postfix[i] == ')')
+            break;
     }
-    printf("\nThe result of expression %s  =  %d\n\n", exp, pop());
+    evalPostfix(postfix);
+
     return 0;
 }
